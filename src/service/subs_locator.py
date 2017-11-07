f3d4dec9a96a30a38c645ac03f9a0c38e546051c
Greg Daneault <gregdaneault@gmail.com>
2017-11-06 23:47:23 -0500

    Fix for changed UA

--- src/service/subs_locator.py
+++ src/service/subs_locator.py
@@ -1,14 +1,22 @@
 import os
 from pythonopensubtitles.opensubtitles import OpenSubtitles
 from pythonopensubtitles.utils import File
+from pythonopensubtitles.settings import Settings
+
+from model import Movie
 
 
 class SubsLocatorService:
-    def __init__(self, username, password):
+    def __init__(self, username, password, user_agent=None):
         if not username or not username.strip():
             raise ValueError("Missing username")
         if not password or not password.strip():
             raise ValueError("Missing password")
+
+        # Allow overriding the user agent since the test UA has changed
+        if user_agent:
+            Settings.USER_AGENT = user_agent
+
         self.os = OpenSubtitles()
         self.os.login(username, password)
 
@@ -20,9 +28,9 @@ class SubsLocatorService:
         """
         movie_file = File(movie.movie_path)
         search_def = {
-            'MovieLanguage': 'English',
-            'MovieHash': movie_file.get_hash(),
-            'MovieByteSize': movie_file.size
+            'sublanguageid': 'eng',
+            'moviehash': movie_file.get_hash(),
+            'moviebytesize': int(movie_file.size)
         }
         subs = self.os.search_subtitles([search_def])
         for sub in subs:
@@ -31,4 +39,5 @@ class SubsLocatorService:
 
 
 if __name__ == "__main__":
-    instance = SubsLocatorService(os.environ.get("OS_USER"), os.environ.get("OS_PASS"))
+    instance = SubsLocatorService(os.environ.get("OS_USER"), os.environ.get("OS_PASS"), os.environ.get("OS_USER_AGENT"))
+    # instance.get_subs_for_movie(m3)
