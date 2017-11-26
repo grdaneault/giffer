308826f32682e95e23493b9513ddce884aa79a0e
Greg Daneault <gregdaneault@gmail.com>
2017-11-26 11:37:58 -0800

    Materialize in progress * Add search box in AppBar and wire it up   * Move url change to willReceiveProps for more consistent interface * Switch to `Select` and `MenuItem` for page selector * Include text of result counts in page selector

--- ui/src/api.js
+++ ui/src/api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const api = axios.create({
     baseURL: 'http://localhost:5000/api/v1/', //http://giffer.greggernaut.com/api/v1/',
-    timeout: 5000,
+    timeout: 15000,
 });
--- ui/src/components/PageSelector.js
+++ ui/src/components/PageSelector.js
@@ -1,22 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {MenuItem, FormControl, InputLabel, Input, Select} from 'material-ui';
 
-const PageSelector = ({ pages, page, onChange }) => {
+const PageSelector = ({ totalResults, pageSize, pages, page, onChange }) => {
 
     const options = [];
     for (let i = 0; i < pages; i++) {
-        options.push(<option value={i} key={i}>Page {i + 1}</option>);
+        options.push(<MenuItem value={i} key={i}>Page {i + 1}</MenuItem>);
     }
+
+    const from = page * pageSize + 1;
+    const to = Math.min(totalResults, (page + 1) * pageSize);
 
     return (
     <span>
-    <select onChange={e => onChange(e.target.value)} value={page}>
-        {options}
-    </select>
+        <span>
+            Viewing results {from} to {to} of {totalResults}
+        </span>
+        <FormControl>
+          <InputLabel htmlFor="age-simple">Jump to page</InputLabel>
+          <Select
+              value={page}
+              onChange={e => onChange(e.target.value)}
+              input={<Input name="page" id="page-simple" />}
+          >
+              {options}
+          </Select>
+        </FormControl>
   </span>
 )};
 
 PageSelector.propTypes = {
+    totalResults: PropTypes.number.isRequired,
+    pageSize: PropTypes.number.isRequred,
     page: PropTypes.number.isRequired,
     pages: PropTypes.number.isRequired,
     onChange: PropTypes.func.isRequired
