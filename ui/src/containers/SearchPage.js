da1be2aca3679745b47a067431fa2878fc9393db
Greg Daneault <gregdaneault@gmail.com>
2017-11-27 19:12:47 -0800

    Rename AppSearch (from material-ui) to SubtitleSearchBox for clarity (streak++)

--- ui/src/containers/SearchPage.js
+++ ui/src/containers/SearchPage.js
@@ -13,7 +13,7 @@ import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
-import AppSearch from "../components/AppSearch";
+import AppSearch from "../components/SubtitleSearchBox";
 
 const styles = theme => ({
     root: {
